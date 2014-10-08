10efb0b937e71df822f84e55a517421459d85828
chenzhepeter <chenzhepeter@gmail.com>
2014-10-08 02:12:22 -0400

    Chapter 2

--- models/post.js
+++ models/post.js
@@ -1,4 +1,5 @@
-var mongodb = require('./db');
+var mongodb = require('./db'),
+  markdown = require('markdown').markdown;
 
 function Post(name, title, post) {
   this.name = name;
@@ -9,23 +10,23 @@ function Post(name, title, post) {
 module.exports = Post;
 
 //存储一篇文章及其相关信息
-Post.prototype.save = function(callback) {
+Post.prototype.save = function (callback) {
   var date = new Date();
   //存储各种时间格式，方便以后扩展
   var time = {
       date: date,
-      year : date.getFullYear(),
-      month : date.getFullYear() + "-" + (date.getMonth() + 1),
-      day : date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate(),
-      minute : date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " +
-      date.getHours() + ":" + (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes())
-  }
-  //要存入数据库的文档
+      year: date.getFullYear(),
+      month: date.getFullYear() + "-" + (date.getMonth() + 1),
+      day: date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate(),
+      minute: date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " +
+        date.getHours() + ":" + (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes())
+    }
+    //要存入数据库的文档
   var post = {
-      name: this.name,
-      time: time,
-      title: this.title,
-      post: this.post
+    name: this.name,
+    time: time,
+    title: this.title,
+    post: this.post
   };
   //打开数据库
   mongodb.open(function (err, db) {
@@ -44,23 +45,23 @@ Post.prototype.save = function(callback) {
       }, function (err) {
         mongodb.close();
         if (err) {
-          return callback(err);//失败！返回 err
+          return callback(err); //失败！返回 err
         }
-        callback(null);//返回 err 为 null
+        callback(null); //返回 err 为 null
       });
     });
   });
 };
 
 //读取文章及其相关信息
-Post.get = function(name, callback) {
+Post.get = function (name, callback) {
   //打开数据库
   mongodb.open(function (err, db) {
     if (err) {
       return callback(err);
     }
     //读取 posts 集合
-    db.collection('posts', function(err, collection) {
+    db.collection('posts', function (err, collection) {
       if (err) {
         mongodb.close();
         return callback(err);
@@ -75,9 +76,12 @@ Post.get = function(name, callback) {
       }).toArray(function (err, docs) {
         mongodb.close();
         if (err) {
-          return callback(err);//失败！返回 err
+          return callback(err); //失败！返回 err
         }
-        callback(null, docs);//成功！以数组形式返回查询的结果
+        docs.forEach(function (doc) {
+          doc.post = markdown.toHTML(doc.post);
+        });
+        callback(null, docs); //成功！以数组形式返回查询的结果
       });
     });
   });
