a933c66fe8d8273ebc1ce03c534a23130e4b36c8
chenzhepeter <chenzhepeter@gmail.com>
2014-10-12 02:18:37 -0400

    Chapter 10

--- models/post.js
+++ models/post.js
@@ -29,7 +29,8 @@ Post.prototype.save = function (callback) {
     title: this.title,
     post: this.post,
     tags: this.tags,
-    comments: []
+    comments: [],
+    pv: 0
   };
   //打开数据库
   mongodb.open(function (err, db) {
@@ -98,7 +99,7 @@ Post.getTen = function (name, page, callback) {
 };
 
 //获取一篇文章
-Post.getOne = function (name, day, title, callback) {
+Post.getOne = function(name, day, title, callback) {
   //打开数据库
   mongodb.open(function (err, db) {
     if (err) {
@@ -116,18 +117,31 @@ Post.getOne = function (name, day, title, callback) {
         "time.day": day,
         "title": title
       }, function (err, doc) {
-        mongodb.close();
         if (err) {
+          mongodb.close();
           return callback(err);
         }
-        //解析 markdown 为 html
         if (doc) {
+          //每访问 1 次，pv 值增加 1
+          collection.update({
+            "name": name,
+            "time.day": day,
+            "title": title
+          }, {
+            $inc: {"pv": 1}
+          }, function (err) {
+            mongodb.close();
+            if (err) {
+              return callback(err);
+            }
+          });
+          //解析 markdown 为 html
           doc.post = markdown.toHTML(doc.post);
           doc.comments.forEach(function (comment) {
             comment.content = markdown.toHTML(comment.content);
           });
+          callback(null, doc);//返回查询的一篇文章
         }
-        callback(null, doc); //返回查询的一篇文章
       });
     });
   });
